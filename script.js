8b82488c152a5327e767a85215cfa13539743992
Zeynep-EGE <zeynepegekaracabey@gmail.com>
2025-02-25 17:28:49 +0300

    hover

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnActive = document.getElementsByClassName('btn');
+
+btnActive.addEventListener('click', function(){
+    if(btnActive.style.backgroundColor === '#be9c79'){
+        btnActive.style.backgroundColor = '#333'
+    }else{
+        btnActive.style.backgroundColor === '#be9c79'
+    }
+});
+
